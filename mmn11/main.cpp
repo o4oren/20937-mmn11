b236e7d7d531dce0abcd2c4707b473411dc3692e
Oren Geva <o4oren@gmail.com>
2021-11-09 13:45:12 +0200

    Initial Commit

---
+++ mmn11/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mmn11
+//
+//  Created by Oren Geva on 09/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
